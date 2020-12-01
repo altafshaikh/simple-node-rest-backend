bfba9736929d8bd0f4c837cc2e5a4944bb2c4404
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-01 15:55:35 +0530

    basic server is created to store the post data

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+var users = { altafshaikh: [], altaf: [] };
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/:username", (req, res) => {
+  res.send("Hello World, from express");
+});
+
+app.post("/user", (req, res) => {
+  const user = req.body;
+
+  // Output the book to the console for debugging
+  console.log(user);
+  const username = user["userName"];
+  users[username] = [user];
+  res.send("User is added to the database");
+});
+
+app.get("/:username/users", (req, res) => {
+  let username = req.params.username;
+  res.json(users[username]);
+});
+
+app.listen(port, () =>
+  console.log(`Hello world app listening on port ${port}!`)
+);
